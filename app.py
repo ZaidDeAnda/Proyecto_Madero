67631077d78cd37c0ea05423e42b2bae4897734f
ZaidTheJedi <zaid_hellscream@hotmail.com>
2019-08-24 21:06:14 -0500

    cambios generales

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from dbutils import MONGO_URI
 from dbutils import db_connect
 from dbutils import db_insert_user
 from dbutils import db_find_all
+from dbutils import db_delete_temp
 from form import BuyForm
 from form import LoginForm
 from flask import Flask
@@ -41,19 +42,15 @@ def index():
 def login():
     form = BuyForm(request.form)
     login_error = False
+    list_temporal= []
     if request.method == 'POST':
         item = form.item.data
         precio = form.precio.data
         if item != '' and precio != '':
-            user = {
-                "item": item,
-                "precio": precio
-            }
-            db_insert_user(users, user)
-            registro = db_find_all(users)
+            list_temporal.append([item, precio])
             flag = True
-            return render_template('index.html', users = registro)
-    return render_template('index.html', login_error=login_error, users= registro)
+            return render_template('index.html', list_temporal = list_temporal)
+    return render_template('index.html', login_error=login_error)
 
 
 @app.errorhandler(404)
