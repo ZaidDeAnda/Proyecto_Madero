cccdf1ea9fb42ef40207d19f14d98234f2708e3b
ZaidTheJedi <zaid_hellscream@hotmail.com>
2019-08-26 20:54:14 -0500

    Se regresó al estado donde funcionaba
    Db_inser_user no funciona por algún motivo.

--- app.py
+++ app.py
@@ -12,35 +12,11 @@ from flask import render_template
 app = Flask(__name__)
 users = db_connect(MONGO_URI, 'Proyecto_Madero', 'users')
 
-
-@app.route('/admin', methods=['GET', 'POST'])
-def index():
-    form = BuyForm(request.form)
-    flag = False
-    name = None
-
-    if len(form.errors):
-        print(form.errors)
-    if request.method == 'POST':
-        item = form.item.data
-        precio = form.precio.data
-        if item != '' and precio != '':
-            print(f"Nombre capturado: {item}")
-            print(f"Email capturado: {precio}")
-            user = {
-                "item": item,
-                "precio": precio
-            }
-            db_insert_user(users, user)
-            flag = True
-
-    return render_template('index.html', flag=flag, name=name)
-
-
 @app.route('/', methods=['GET', 'POST'])
 def login():
     form = BuyForm(request.form)
     login_error = False
+    registro={}
     if request.method == 'POST':
         item = form.item.data
         precio = form.precio.data
@@ -49,11 +25,11 @@ def login():
                 "item": item,
                 "precio": precio
             }
-            db_insert_user(users, user)
-            registro = db_find_all(users)
+            #db_insert_user(users, user)
+            #registro = db_find_all(users)
             flag = True
             return render_template('index.html', users = registro)
-    return render_template('index.html', login_error=login_error, users= registro)
+    return render_template('index.html', login_error=login_error)
 
 
 @app.errorhandler(404)
